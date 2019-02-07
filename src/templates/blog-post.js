4c4b62ab2f843464302b1a40dcf4e1e3be7a5141
Tim Smith <tgsmith489@gmail.com>
2019-02-06 21:16:22 -0600

    Changed blog post excerpt length to 160 characters

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -77,7 +77,7 @@ export const query = graphql`
   query BlogQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
-      excerpt(pruneLength: 200)
+      excerpt(pruneLength: 160)
       fields {
         slug
       }
